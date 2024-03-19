d3352df1ac3b66c993d0decbd4c54d0994875d7d
Charmtiger <charmtiger@gmail.com>
2024-03-20 00:57:13 +0800

    feat: auto create router & package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
 import LoadingOrError from "./components/LoadingOrError";
 import { lazy, Suspense, useEffect } from "react";
-import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { history } from "@/utils/router";
-import { componentDidMount } from '@/utils/utils';
-import '@/utils/portal';
-
+import { componentDidMount } from "@/utils/utils";
+import "@/utils/portal";
 const isHttps = document.location.protocol === "https:";
+
 if (import.meta?.env?.MODE !== "development" && !isHttps) {
   // window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-}
-
-// import VConsole from 'vconsole'
+} // import VConsole from 'vconsole'
 // const vConsole = new VConsole({ theme: 'dark' })
 
-
 export default function App() {
-
   useEffect(() => {
     componentDidMount?.();
   }, []);
-
   return (
     <HashRouter history={history}>
       <Suspense fallback={<LoadingOrError />}>
-        <Switch>
-        </Switch>
+        <Routes>
+        </Routes>
       </Suspense>
     </HashRouter>
   );
